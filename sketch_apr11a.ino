50570ec58c2e297e8070d00e13e409796f0cd408
blueridge2 <chip.from.nc@gmail.com>
2020-01-05 15:25:42 -0700

    changes the # to a comma for parsing

--- sketch_apr11a.ino
+++ sketch_apr11a.ino
@@ -110,7 +110,7 @@ char *gps_parsed_data[ARRAY_SIZE];
 // you need to know the rules in your country.
 // the following data structures do not need to be reallocated every time
 //                                           0123456
-char radiopacket[RH_RF69_MAX_MESSAGE_LEN] = "kf4wbk#";
+char radiopacket[RH_RF69_MAX_MESSAGE_LEN] = "kf4wbk,";
 char gps_data[GPS_RECEIVER_BUFFER_SIZE];
 
 void setup()
@@ -286,19 +286,6 @@ void loop() {
     if (rf69_manager.sendtoWait((uint8_t *)radiopacket, strlen(radiopacket), DEST_ADDRESS)) {
         // Now wait for a reply from the server
         reply_buffer_len = sizeof(reply_buffer);
-        Serial.print("got an ack");
-        //uint8_t from;
-        //if (rf69_manager.recvfromAckTimeout(reply_buffer, &reply_buffer_len, 2000, &from)) {
-        //    reply_buffer[reply_buffer_len] = 0; // zero out remaining string
-       //     Serial.print("Got reply from #"); Serial.print(from);
-       //     Serial.print(" [RSSI :");
-        //    Serial.print(rf69.lastRssi());
-        //    Serial.print("] : ");
-        //    Serial.println((char*)reply_buffer);
-        //    Blink(LED, 100, 3); //blink LED 3 times, 40ms between blinks
-        //} else {
-        //    Serial.println("No reply, is anyone listening?");
-        //}
     } else {
         Serial.println("Sending failed (no ack)");
     }
